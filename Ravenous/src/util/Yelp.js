40b667e33411fcfdb7e1745a221d2d6337b82cc1
Jeannie Thai <50002031+Jeannie-Thai@users.noreply.github.com>
2019-06-01 11:22:20 -0400

    Add files via upload

---
+++ Ravenous/src/util/Yelp.js
@@ -0,0 +1,34 @@
+const apiKey = '96u3buzFEFvuwxv61r-D2c-dNP32k5wsEEur7kR93ckCNCjjmMytLTQSRNZAf5IKHUBtKmeO3-3yT35ogWSJ8bSxyPSD86ICYGn8Iguxe5841pj6CSsm4w6J0ILyXHYx'
+
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+        { 
+            headers: {
+            Authorization: `Bearer ${apiKey}` ,
+            },
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(((business) => {
+                    console.log(business);
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                    };
+                }));
+            }
+        })
+    }
+};
+
+export default Yelp;
